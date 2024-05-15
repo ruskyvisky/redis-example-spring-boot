4b5132d57e06093ee35598d1484b1a10a5e0b1c3
ruskyvisky <numan_859@hotmail.com>
2024-05-15 20:28:20 +0300

    added CRUD Operations with cacheable configurations

--- src/main/java/com/example/redis/demo/controller/UserController.java
+++ src/main/java/com/example/redis/demo/controller/UserController.java
@@ -1,11 +1,48 @@
 package com.example.redis.demo.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.redis.demo.dto.CreateUserDto;
+import com.example.redis.demo.dto.UpdateUserDto;
+import com.example.redis.demo.model.User;
+import com.example.redis.demo.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto){
+        return ResponseEntity.ok(userService.createUser(createUserDto));
+    }
+
+    @GetMapping("/getall")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.deleteUserById(id));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUser(@RequestBody UpdateUserDto dto){
+        return ResponseEntity.ok(userService.updateUser(dto));
+    }
+
+
 
 }
